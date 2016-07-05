fa741a9c4560a7f26da48842667fa9f95a1213f9
Roman Muentener <munt@zhaw.ch>
2016-07-05 09:31:05 +0200

    * cache a list of all conditions

--- ptoweb/api/__init__.py
+++ ptoweb/api/__init__.py
@@ -4,6 +4,40 @@ import json
 from ptoweb.api.auth import require_auth
 import re
 
+
+def condition_exists(name):
+
+  if name in enumerate_conditions():
+    return True
+  else:
+    return False
+
+
+def enumerate_conditions():
+
+  dic = get_from_cache('all_conditions')
+  if(dic != None):
+    return dic
+  
+  observations = get_observations_collection()
+  conditions = observations.aggregate([{'$unwind' : '$conditions'} , {'$group' : { '_id' : "$conditions"}}])
+  dic = {}
+  for condition in conditions:
+    dic[condition['_id']] = True
+  
+  put_to_cache('all_conditions', dic)
+
+  return dic
+
+
+def get_from_cache(key):
+  return cache.get(key)
+
+
+def put_to_cache(key, value):
+  cache.set(key, value, timeout = (app.config['CACHE_TIMEOUT']))
+
+
 def json200(obj):
   return Response(json.dumps(obj), status=200, mimetype='application/json')
 
@@ -22,24 +56,37 @@ def api_conditions_total():
   observations = get_observations_collection()
 
   conditions = list(map(lambda a: a.strip(), request.args.get('name').split(',')))
+  
+  conditions_filtered = []
 
-  cache_key = '/api/conditions_total/' + ','.join(conditions)
+  non_existing = []
 
-  result = cache.get(cache_key)
+  for condition in conditions:
+    if not condition_exists(condition):
+      non_existing.append({'_id' : condition, 'count' : -1})
+    else:
+      conditions_filtered.append(condition)
+
+  conditions_filtered = sorted(conditions_filtered)
+    
+
+  cache_key = '/api/conditions_total/' + ','.join(conditions_filtered)
+
+  result = get_from_cache(cache_key)
   if result != None:
-    return json200(result)
+    return json200(result + non_existing)
 
   pipeline = [
     {'$unwind' :'$conditions'}, 
-    {'$match' : {'conditions' : {'$in' : conditions}}},
+    {'$match' : {'conditions' : {'$in' : conditions_filtered}}},
     {'$group' : {'_id' : '$conditions', 'count': {'$sum' : 1}}},
     {'$sort' : {'count' : -1}}
    ]
 
   result = list(observations.aggregate(pipeline))
-  cache.set(cache_key, result, timeout = 60*60*60)
+  put_to_cache(cache_key, result)
 
-  return json200(result)
+  return json200(result + non_existing)
 
 
 @app.route('/api/conditions')
@@ -56,7 +103,7 @@ def api_conditions():
 
   cache_key = '/api/conditions/' + dip + '/' + sip
 
-  result = cache.get(cache_key)
+  result = get_from_cache(cache_key)
   if result != None:
     return json200(result)
   
@@ -79,7 +126,7 @@ def api_conditions():
     ]
 
   result = list(uploads.aggregate(pipeline, allowDiskUse = True))
-  cache.set(cache_key, result, timeout = 60*60*60)
+  put_to_cache(cache_key, result)
 
   return json200(result)
 
